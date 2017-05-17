2154a0cb6526c1aa64df772ee0af686d33810c71
jourdelg <jourdelg@gmail.com>
2017-05-17 10:44:51 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var song;
 
 function setup() {
-  song = loadSound('final/[2009] WattsonHi - Serenade.mp3);
+  song = loadSound('/[2009] WattsonHi - Serenade.mp3);
   createCanvas(720, 200);
   background(255,0,0);
 }
