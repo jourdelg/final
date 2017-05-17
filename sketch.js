761d21fe789e865bd6c6e1619660b5e350295d18
jourdelg <jourdelg@gmail.com>
2017-05-17 10:43:24 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,18 @@
+r song;
+
+function setup() {
+  song = loadSound('final/[2009] WattsonHi - Serenade.mp3);
+  createCanvas(720, 200);
+  background(255,0,0);
+}
+
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+    background(255,0,0);
+  } else {
+    song.play();
+    background(0,255,0);
+  }
+}
 
