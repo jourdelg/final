22ed6296f5a4bde2c60fdb808af4fcab453b2c4a
jourdelg <jourdelg@gmail.com>
2017-05-17 10:52:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 var song;
+var img;  // Declare variable 'img'.
 
 function setup() {
-  song = loadSound('assets/[2009] WattsonHi - Serenade.mp3');
+  createCanvas(720, 400);
+  img = loadImage("musicbot.jpg");  // Load the image
+}
+
+function draw() {
+  // Displays the image at its actual size at point (0,0)
+  image(img, 0, 0);
+  // Displays the image at point (0, height/2) at half size
+  image(img, 0, height/2, img.width/2, img.height/2);
+}
+
+
+function setup() {
+  song = loadSound('[2009] WattsonHi - Serenade.mp3');
   createCanvas(720, 200);
   background(255,0,0);
 }
